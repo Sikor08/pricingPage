0300b44dd6077e704a05acbc75d15a814887b87e
Yury <92983534+Sikor08@users.noreply.github.com>
2024-01-29 17:19:36 +0300

    adaptive complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './styles/styles.css';
+const hamIcon = document.querySelector('.ham-icon');
+const hamList = document.querySelector('.ham-list');
+
+hamIcon.addEventListener('click', () => {
+    hamList.classList.toggle('ham-list__active');
+    hamIcon.classList.toggle('ham-icon__active');
+})
+
+const card = document.querySelectorAll('.tariff-card');
+    card[1].classList.add('tariff-card_active');
+card.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.classList.add('tariff-card_active');
+        card[1].classList.remove('tariff-card_active')
+        card[1].addEventListener('mouseover', ()=> {
+            card[1].classList.add('tariff-card_active')
+        })
+    });
+    item.addEventListener('mouseout', () => {
+        item.classList.remove('tariff-card_active');
+        card[1].classList.add('tariff-card_active')
+    })
+
+})
